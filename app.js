f3d24d2d035c0a6bd2813f3f8c6e815f516a9111
soucannabisdev <webmaster@soucannabis.ong.br>
2023-02-18 11:41:50 -0300

    deploy

--- app.js
+++ app.js
@@ -1,28 +1,68 @@
 const https = require('https'); // or 'https' for https:// URLs
 const fs = require('fs');
+const axios = require("axios");
+const { url } = require('inspector');
 
-optionsPipe = {
-    method: 'POST',
-    headers: {
-        'accept': 'application/json',
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDE5OTU2ODEsImVtYWlsIjoiZmVsaXBlcm9zZW5la0BnbWFpbC5jb20iLCJhcHBsaWNhdGlvbiI6MzAwMTQyMDIwfX0.JugAF92MqbUV_fLVKEcF5jUI3G4G2hlAmLeBJ-dEfsEIlX3gdKO1IfbQRUYvHvAk569vuD9K_zCrKylY6R6agw',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        query: 'mutation { exportPipeReport(input: {pipeId: 301587647, pipeReportId: 300369040}) { pipeReportExport { id fileURL report { id } } } }'
+var urlReport = ''
 
-    })
-};
 
-https.post('https://api.pipefy.com/graphql', optionsPipe)
-.then(res => res.json())
-.then(json => {
-    console.log(json)
- })
-.catch(err => console.log(err))
+async function app (){
+
+    var idReport = ''
+
+   /* optionsPipe = {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDE5OTU2ODEsImVtYWlsIjoiZmVsaXBlcm9zZW5la0BnbWFpbC5jb20iLCJhcHBsaWNhdGlvbiI6MzAwMTQyMDIwfX0.JugAF92MqbUV_fLVKEcF5jUI3G4G2hlAmLeBJ-dEfsEIlX3gdKO1IfbQRUYvHvAk569vuD9K_zCrKylY6R6agw',
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            query: 'mutation { exportPipeReport(input: {pipeId: 301587647, pipeReportId: 300369040}) { pipeReportExport { id fileURL report { id } } } }'
+
+        })
+    };
+
+    await axios('https://api.pipefy.com/graphql', optionsPipe)
+      .then(function (response) {
+        idReport = response.data.data.exportPipeReport.pipeReportExport
+        return idReport
+      })
+      .catch(function (error) {
+        console.log(error);
+      });*/
+
+      optionsPipe = {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDE5OTU2ODEsImVtYWlsIjoiZmVsaXBlcm9zZW5la0BnbWFpbC5jb20iLCJhcHBsaWNhdGlvbiI6MzAwMTQyMDIwfX0.JugAF92MqbUV_fLVKEcF5jUI3G4G2hlAmLeBJ-dEfsEIlX3gdKO1IfbQRUYvHvAk569vuD9K_zCrKylY6R6agw',
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            query: '{ pipeReportExport(id: 304165464) { fileURL state startedAt requestedBy { id } } }'
+
+        })
+    };
+
+    await axios('https://api.pipefy.com/graphql', optionsPipe)
+      .then(function (response) {
+        urlReport = response.data.data.pipeReportExport.fileURL
+   
+        return urlReport
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    
+   
+      
+}
+
+app()
 
 const file = fs.createWriteStream("relatorio.xlsx");
-https.get("https://app-storage-service.pipefy.com/v1/signed/orgs/0ec0a956-43d7-4305-8b15-0faa0a513586/reports/94176a02-c43c-472f-93bb-f5c150149b8c/pedidos_concluidos_18-02-2023.xlsx?expires_on=1676729981&signature=X%2B%2Fk%2FZHQ4ZudIW5nVOFXiGSECbvUcPl6cZltWoGn%2BpU%3D", function(response) {
+https.get("https://app-storage-service.pipefy.com/v1/signed/orgs/0ec0a956-43d7-4305-8b15-0faa0a513586/reports/94176a02-c43c-472f-93bb-f5c150149b8c/pedidos_concluidos_18-02-2023.xlsx?expires_on=1676734416&signature=tSk6op7xtjYXUTASMnqCsyUBTBu7nRcjVtfodR9%2BOYI%3D", function(response) {
    response.pipe(file);
 
    // after download completed close filestream
