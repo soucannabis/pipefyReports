b1b890c140f9ea74d2c17d13a5ed9fc9a1ff6708
soucannabisdev <webmaster@soucannabis.ong.br>
2023-03-03 11:24:22 -0300

    deploy

--- app.js
+++ app.js
@@ -6,20 +6,16 @@ api.use(express.json());
 api.use(express.urlencoded({
   extended: true
 }));
-const https = require('https');
-const fs = require('fs');
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const pipePedidos = require("./routes/pipe-pedidos")
 const pipeLiga = require("./routes/pipe-pedidos")
 const pipeAssociados = require("./routes/pipe-pedidos")
 const pipeServicos = require("./routes/pipe-pedidos")
 
-api.use("/", pipePedidos)
-api.use("/", pipeLiga)
-api.use("/", pipeAssociados)
-api.use("/", pipeServicos)
+api.use("/pipe-pedidos", pipePedidos)
+api.use("/pipe-liga", pipeLiga)
+api.use("/pipe-associados", pipeAssociados)
+api.use("/pipe-servicos", pipeServicos)
 
   api.listen(process.env.PORT || 3000, () => {
       console.log('API RUN!');
