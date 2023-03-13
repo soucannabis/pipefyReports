0121bbb5ff5a2731b6127218d620b8ae5246888f
soucannabisdev <webmaster@soucannabis.ong.br>
2023-03-13 15:32:30 -0300

    deploy

--- routes/pipe-associados.js
+++ routes/pipe-associados.js
@@ -8,113 +8,20 @@ const puppeteer = require('puppeteer');
 const bodyParser = require('body-parser')
 const multer = require('multer');
 
-
-
-router.get('/novo-relatorio', async (req, res) => {
-    var idReport = ''
-    var urlReport = ''
-    var pipeId = 301738151
-    var pipeReporId = 300427413
-  
-   optionsPipe = {
-        method: 'POST',
-        headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDE5OTU2ODEsImVtYWlsIjoiZmVsaXBlcm9zZW5la0BnbWFpbC5jb20iLCJhcHBsaWNhdGlvbiI6MzAwMTQyMDIwfX0.JugAF92MqbUV_fLVKEcF5jUI3G4G2hlAmLeBJ-dEfsEIlX3gdKO1IfbQRUYvHvAk569vuD9K_zCrKylY6R6agw',
-            'Content-Type': 'application/json'
-        },
-        data: JSON.stringify({
-            query: 'mutation { exportPipeReport(input: {pipeId: '+pipeId+', pipeReportId: '+pipeReporId+'}) { pipeReportExport { id fileURL report { id } } } }'
-  
-        })
-    };
-  
-    await axios('https://api.pipefy.com/graphql', optionsPipe)
-      .then(function (response) {
-        idReport = response.data.data.exportPipeReport.pipeReportExport.id
-        return idReport
-      })
-      .catch(function (error) {
-        console.log(error);
-      });  
-  
-      await delay(10000)
-   
-      optionsPipe = {
-        method: 'POST',
-        headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDE5OTU2ODEsImVtYWlsIjoiZmVsaXBlcm9zZW5la0BnbWFpbC5jb20iLCJhcHBsaWNhdGlvbiI6MzAwMTQyMDIwfX0.JugAF92MqbUV_fLVKEcF5jUI3G4G2hlAmLeBJ-dEfsEIlX3gdKO1IfbQRUYvHvAk569vuD9K_zCrKylY6R6agw',
-            'Content-Type': 'application/json'
-        },
-        data: JSON.stringify({
-            query: '{ pipeReportExport(id: '+idReport+') { fileURL state startedAt requestedBy { id } } }'
-  
-        })
-    };
-  
-    await axios('https://api.pipefy.com/graphql', optionsPipe)
-      .then(function (response) {
-  
-        urlReport = response.data.data.pipeReportExport.fileURL
-  
-        return urlReport
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  
-      file = fs.createWriteStream("relatorio-associados.xlsx");
-     
-        https.get(urlReport,  function(response) {
-          response.pipe(file);
-  
-          file.on("finish", () => {
-              file.close();
-              console.log("NOVO RELATÓRIO CRIADO!   - "+idReport+" - "+req.body.hour+":"+req.body.minute+"h");
-              console.log(urlReport)
-              console.log()
-          });
-        });
-        res.send("<h4>Relatório criado com sucessso!</h4>")
-  
-        res.status(200).end()
-   
-});
-
 router.get('/', async (req, res) => {   
- /* var ano = new Date().getFullYear()
-  var mes = new Date().getMonth()
-  mes = mes+1
-  var dia = new Date().getDate()
-  if(mes <= 9){mes = "0"+mes}
-  if(dia <= 9){dia = "0"+dia}
-
-  var date = dia + "-" + mes + "-" + ano
-  const xlsx = __dirname+"/novo_relatório_"+date+".xlsx";*/
-
-  res.download(__dirname+"/uploads/relatorio.xlsx")
-
-//   res.download(xlsx)
-
-})
-
-router.get('/pipe-report', async (req, res) => {    
-  const xlsx = `relatorio-associados.xlsx`;
-   res.download(xlsx)
-
+  res.download(__dirname+"/uploads/pipe-associados/relatorio.xlsx")
 })
 
 const storage = multer.diskStorage({
   destination: function(req,file,cb){
-    cb(null, "routes/uploads/")
+    cb(null, "routes/uploads/pipe-associados")
   },
   filename: function(req,file,cb){
     cb(null, "relatorio.xlsx")
   }
 })
 
-const upload = multer({storage}); // define o diretório onde os arquivos serão armazenados
+const upload = multer({storage}); 
 
 router.post('/upload', upload.single('arquivo'), (req, res) => {
   res.send('Arquivo enviado com sucesso!');
@@ -122,93 +29,6 @@ router.post('/upload', upload.single('arquivo'), (req, res) => {
 
 router.get('/upload', async (req, res) => {    
   res.send('<form action="upload" method="POST" enctype="multipart/form-data"><input type="file" name="arquivo"><button type="submit">Enviar</button></form>')
-
 })
-
-
-
-/*
-router.get('/pipe-report', async (req, res) => {  
-
-  res.status(200)
- 
-  async function app(){
-    try{
-      process.setMaxListeners(Infinity)
-        let options = {
-            defaultViewport: {
-              width: 1366,
-              height: 768,
-            },
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            ignoreDefaultArgs: ['--disable-extensions'],
-            headless: true,
-          };
-
-          console.log("Abrindo Browser")
-        
-        let browser = await puppeteer.launch(options);
-        let page = await browser.newPage();
-  
-        const client = await page.target().createCDPSession()
-        await client.send('Page.setDownloadBehavior', {
-         behavior: 'allow',
-        downloadPath: __dirname,
-        })
-
-        console.log("Abrindo Pipefy")
-          await page.goto("https://app.pipefy.com/") 
-          await page.waitForSelector(".auth0-lock-submit")   
-          await delay(3000) 
-          var usernameInput = await page.$("input[name='username']");
-          var passwordInput = await page.$("input[name='password']");
-  
-          await delay(4000)
-  
-          await usernameInput.click();
-          await page.keyboard.type("feliperosenek@gmail.com", {
-            delay: 100
-          });
-          await passwordInput.click();
-          await page.keyboard.type("feliperosene2130", {
-            delay: 100
-          });
-  
-          var loginButton = await page.$("button[name='submit']");
-          await loginButton.click()
-
-          console.log("Login OK")
-  
-          await delay(6000)
-  
-          await page.goto("https://app.pipefy.com/apollo_databases/301585673")
-  
-          await delay(10000)
-  
-          var exportButton = await page.$("button[aria-label='Exportar']");
-          await exportButton.click()   
-          
-          console.log("Baixando relatório")
-          
-          await delay(10000)
-  
-          browser.close()
-
-          console.log("Download OK")
-          console.log("Browser fechado")
-
-          res.send("Relatório do database exportado com sucesso!")
-          res.status(200)
-       
-    }
-    catch (error) {
-        console.log(error);
-        process.exit(1);
-      }
-  }
-  
-  app()
-
-})*/
 
 module.exports = router
